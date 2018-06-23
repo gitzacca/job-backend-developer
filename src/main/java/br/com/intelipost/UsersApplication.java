6711721e14b15f9c3be16617f0ebaa107508d47e
Bruno Zaccaria Marques <zaccabruno@gmail.com>
2018-06-23 15:59:10 -0300

    Implementado a estrutura inicial do módulo de autenticação.

--- src/main/java/br/com/intelipost/UsersApplication.java
+++ src/main/java/br/com/intelipost/UsersApplication.java
@@ -1,4 +1,4 @@
-package br.com.intelipost.users;
+package br.com.intelipost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/intelipost/UsersApplicationTests.java
+++ src/test/java/br/com/intelipost/UsersApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.intelipost.users;
+package br.com.intelipost;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
