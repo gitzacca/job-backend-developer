6095a475be1c437c4ae008587aa1e141dbea6ff1
Bruno Zaccaria <zaccabruno@gmail.com>
2018-06-26 12:19:59 -0300

    Implementado variaveis na configuração do Redis para que o host e porta possa ser parametrizado.

--- src/main/java/br/com/intelipost/config/RedisConfig.java
+++ src/main/java/br/com/intelipost/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package br.com.intelipost.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -8,8 +9,17 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @Value("${config.redis.host}")
+    private String host;
+
+    @Value("${config.redis.port}")
+    private Integer port;
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.getStandaloneConfiguration().setHostName(host);
+        jedisConnectionFactory.getStandaloneConfiguration().setPort(port);
         return new JedisConnectionFactory();
     }
 
